5f5f185cd3a6cd0d8b7d130f259f259b721b797c
ronnasqueroquitar <ronnasayd.machado@queroquitar.com.br>
2023-12-09 13:01:06 -0300

    refactor: update

--- get.sh
+++ get.sh
@@ -3,3 +3,4 @@ cp ~/.zshrc $(pwd)/zsh/.zshrc
 cp ~/.vimrc $(pwd)/vim/.vimrc
 cp ~/.p10k.zsh $(pwd)/p10k/.p10k.zsh
 cp ~/.tmux.conf $(pwd)/tmux/.tmux.conf
+cp ~/.config/terminator/config $(pwd)/terminator/config
