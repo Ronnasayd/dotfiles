ad0251f16554ed9f7fbca9347238f437cf824041
ronnasqueroquitar <ronnasayd.machado@queroquitar.com.br>
2023-12-12 19:52:42 -0300

    updates

--- get.sh
+++ get.sh
@@ -3,4 +3,5 @@ cp ~/.zshrc $(pwd)/zsh/.zshrc
 cp ~/.vimrc $(pwd)/vim/.vimrc
 cp ~/.p10k.zsh $(pwd)/p10k/.p10k.zsh
 cp ~/.tmux.conf $(pwd)/tmux/.tmux.conf
+cp ~/.conkyrc $(pwd)/conky/.conkyrc
 cp ~/.config/terminator/config $(pwd)/terminator/config
