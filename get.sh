9d692da1bb523f1bd2727f3b573ef018b2c024df
Ronnasayd <ronnasayd@hotmail.com>
2024-07-31 16:23:12 -0300

    refactor: update

--- get.sh
+++ get.sh
@@ -6,6 +6,7 @@ cp ~/.conkyrc $(pwd)/conky/.conkyrc
 cp ~/.config/terminator/config $(pwd)/terminator/config
 cp ~/.vim/coc-settings.json $(pwd)/vim/coc-settings.json
 cp ~/.coc.vim $(pwd)/vim/.coc.vim
+cp ~/.gitconfig $(pwd)/git/.gitconfig
 dconf dump / > $(pwd)/gnome/dconf.gnome.bkp
 # copy mymimosa
 cp -r ~/.config/conky/MyMimosa/ $(pwd)/conky/themes/MyMimosa
