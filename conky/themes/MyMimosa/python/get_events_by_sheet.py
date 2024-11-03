2e0040f95dbb9a74e97cf836362f0e23e2087fe8
Ronnasayd <ronnasayd@hotmail.com>
2024-11-03 08:45:26 -0300

    refactor: update

--- conky/themes/MyMimosa/python/get_events_by_sheet.py
+++ conky/themes/MyMimosa/python/get_events_by_sheet.py
@@ -2,20 +2,30 @@ import requests
 import datetime
 
 def main():
-  with open("/tmp/conky-calendar", "w") as file:
-    response = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5o7NI3KjKdq_yxt8dmFbIuj8o5mlw4cdlv3Qi-IcBw4HBBeQx4UPbRE4tiqjV-hGu6on3kJ2e2dHo/pub?output=csv")
-    lines  = response.text.split("\n")
-    for line in lines[1:]:
-      terms = line.split(",")
-      title = terms[1][:30]
-      start = terms[2]
-      end = terms[3]
-      # description = terms[4]
-      start = datetime.datetime.strptime(start,"%d/%m/%Y %H:%M:%S").strftime("%d/%m %H:%M")
-      end = datetime.datetime.strptime(end,"%d/%m/%Y %H:%M:%S").strftime("%H:%M")
-      line = f"offset|{start} to {end} - {title}\n"
-      file.write(line)
-      print(line)
+  response = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5o7NI3KjKdq_yxt8dmFbIuj8o5mlw4cdlv3Qi-IcBw4HBBeQx4UPbRE4tiqjV-hGu6on3kJ2e2dHo/pub?output=csv")
+  lines  = response.text.split("\n")
+  now = datetime.datetime.now()
+  counter = 0
+  if len(lines) > 1:
+    with open("/tmp/conky-calendar", "w") as file:
+      for line in lines[1:]:
+        if counter > 4:
+          break
+        terms = line.split(",")
+        title = terms[1][:30]
+        start_raw = terms[2]
+        end_raw = terms[3]
+        # description = terms[4]
+        start = datetime.datetime.strptime(start_raw,"%d/%m/%Y %H:%M:%S")
+        start_string = start.strftime("%d/%m %H:%M")
+        end = datetime.datetime.strptime(end_raw,"%d/%m/%Y %H:%M:%S")
+        end_string = end.strftime("%H:%M")
+        if now < end:
+          counter += 1
+          line = f"offset|{start_string} to {end_string} - {title}\n"
+          file.write(line)
+          print(line)
+      file.write("\n"*max(4 - counter,0))
 
 if __name__ == "__main__":
   main()
