f01b8b7943923abd65e76a45854e7e1f9fab42ee
Ronnasayd <ronnasayd@hotmail.com>
2024-11-08 14:45:13 -0300

    refactor: update

--- conky/themes/MyMimosa/python/get_events_by_sheet.py
+++ conky/themes/MyMimosa/python/get_events_by_sheet.py
@@ -6,26 +6,30 @@ def main():
   lines  = response.text.split("\n")
   now = datetime.datetime.now()
   counter = 0
+  content = ""
   if len(lines) > 1:
-    with open("/tmp/conky-calendar", "w") as file:
-      for line in lines[1:]:
-        if counter > 4:
-          break
-        terms = line.split(",")
-        title = terms[1][:30]
-        start_raw = terms[2]
-        end_raw = terms[3]
-        # description = terms[4]
-        start = datetime.datetime.strptime(start_raw,"%d/%m/%Y %H:%M:%S")
-        start_string = start.strftime("%d/%m %H:%M")
-        end = datetime.datetime.strptime(end_raw,"%d/%m/%Y %H:%M:%S")
-        end_string = end.strftime("%H:%M")
-        if now < end:
-          counter += 1
-          line = f"offset|{start_string} to {end_string} - {title}\n"
-          file.write(line)
-          print(line)
-      file.write("\n"*max(4 - counter,0))
+    for line in lines[1:]:
+      if counter > 4:
+        break
+      terms = line.split(",")
+      title = terms[1][:30]
+      start_raw = terms[2]
+      end_raw = terms[3]
+      # description = terms[4]
+      start = datetime.datetime.strptime(start_raw,"%d/%m/%Y %H:%M:%S")
+      start_string = start.strftime("%d/%m %H:%M")
+      end = datetime.datetime.strptime(end_raw,"%d/%m/%Y %H:%M:%S")
+      end_string = end.strftime("%H:%M")
+      if now < end:
+        counter += 1
+        line = f"offset|{start_string} to {end_string} - {title}\n"
+        file.write(line)
+        content += line
+        print(line)
+    if counter > 0:
+      with open("/tmp/conky-calendar", "w") as file:
+        file.write(content)
+        file.write("\n"*max(4 - counter,0))
 
 if __name__ == "__main__":
   main()
