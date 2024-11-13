2b8d6e2d43306f517d2c0d68d953ba856e5e0771
Ronnasayd <ronnasayd@hotmail.com>
2024-11-13 12:27:46 -0300

    chore(plank): add new icons

---
+++ scripts/set_icons.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gio set -t string ~/develop metadata::custom-icon file:///home/ronnas/develop/personal/dotfiles/plank-links/icons/Code.png
+gio set -t string /home/ronnas/Pictures/wallpapers metadata::custom-icon file:///home/ronnas/develop/personal/dotfiles/plank-links/icons/Pictures.png
+
