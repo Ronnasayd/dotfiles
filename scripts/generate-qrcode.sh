e8383ef9267eaff934f826d00f5aef017d14f8b7
Ronnasayd <ronnasayd@hotmail.com>
2024-12-18 15:46:53 -0300

    refactor: 🔨 update

--- scripts/generate-qrcode.sh
+++ scripts/generate-qrcode.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-qrencode -o /tmp/qrencode.png "$1" && display /tmp/qrencode.png
+if [ $# -eq 0 ]; then
+    exit 0
+elif [ $# -eq 1 ]; then
+    URL=$1
+else
+    PARAM=$1
+    URL=$2
+fi
+if [ "$PARAM" == "-w" ]; then
+    URL="https://api.whatsapp.com/send/?text=$URL"
+fi
+qrencode -o /tmp/qrencode.png "$URL" && display /tmp/qrencode.png
