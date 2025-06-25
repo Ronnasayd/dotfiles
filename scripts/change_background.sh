210662ece7411d34cee7cc73be62de60e7ec4da4
Ronnasayd <ronnasayd@hotmail.com>
2025-06-25 20:32:37 -0300

    refactor: 🔨 update

--- scripts/change_background.sh
+++ scripts/change_background.sh
@@ -15,6 +15,7 @@ done
 CURBG=$(gsettings get org.cinnamon.desktop.background picture-uri)
 NEXTBG="'file://$(jq -r .next ~/.config/conky/MyMimosa/.cache/stats.json)'"
 if [[ $CURBG == $NEXTBG ]]; then
+  IMAGE_DIR="/home/ronnas/Pictures/wallpapers/images"
   RANDOM_IMAGE=$(find "$IMAGE_DIR" -type f | shuf -n 1)
   NEXTBG="file://$RANDOM_IMAGE"
 fi
